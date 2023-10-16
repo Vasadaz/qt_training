3784703f2707e2c6fcd2f614acf3cd34bc927d95
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2023-10-16 21:52:01 +0300

    Updated first window.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import time
 
 from PyQt6 import QtCore, QtGui, QtWidgets
 
@@ -9,6 +10,25 @@ class MyWin(QtWidgets.QMainWindow):
         QtWidgets.QWidget.__init__(self, parent)
         self.ui = des.Ui_MainWindow()
         self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.handler)
+        self.ui.pushButton_2.clicked.connect(self.setcolor)
+
+    def handler(self):
+        self.ui.pushButton.setDisabled(True)
+        self.ui.plainTextEdit.appendPlainText('text')
+        self.ui.label.setText('DONE!')
+        self.ui.pushButton.setText('STOP')
+        self.ui.pushButton.setStyleSheet('background-color: #E74D25; color: white;')
+        print('work')
+
+
+    def setcolor(self):
+        self.ui.pushButton.setDisabled(False)
+        self.ui.pushButton.setText('GO')
+        self.ui.pushButton.setStyleSheet('background-color: #00E000; color: white;')
+
+
+
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
