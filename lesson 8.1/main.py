64a8ff0ce910c6077d123f54522aab794ce50c18
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2023-11-19 00:24:14 +0300

    DONE lesson 8.1 & 8.2

--- lesson 8.1/main.py
+++ lesson 8.1/main.py
@@ -1,48 +1,62 @@
 import sys
 
-from PyQt6 import QtGui, QtWidgets
+from PyQt6 import QtWidgets
 
 import qt_code
 
 
 class GUI(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
-        QtWidgets.QWidget.__init__(self, parent)
+        super().__init__(parent)
         self.ui = qt_code.Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.pushButton.clicked.connect(self.notifications)
         self.ui.pushButton_2.clicked.connect(self.confirm)
         self.ui.pushButton_3.clicked.connect(self.message)
+        self.ui.pushButton_4.clicked.connect(self.error_message)
 
     def notifications(self):
-        QtWidgets.QMessageBox.about(self, 'title', 'message')
-        QtWidgets.QMessageBox.warning(self, 'title', 'message')
+        # QtWidgets.QMessageBox.about(self, 'title', 'message')
+        # QtWidgets.QMessageBox.warning(self, 'title', 'message')
         QtWidgets.QMessageBox.information(self, 'title', 'message')
-        QtWidgets.QMessageBox.critical(self, 'title', 'message')
+        # QtWidgets.QMessageBox.critical(self, 'title', 'message')
 
     def confirm(self):
         result = QtWidgets.QMessageBox.question(
             self,
             'Headers',
             'Description...',
-            QtWidgets.QMessageBox.Yes,
-            QtWidgets.QMessageBox.No,
-            #QtWidgets.QMessageBox.,
+            QtWidgets.QMessageBox.StandardButton.Yes,
+            QtWidgets.QMessageBox.StandardButton.No,
+            # QtWidgets.QMessageBox.,
         )
 
-    def message(self):
+        if result == QtWidgets.QMessageBox.StandardButton.Yes:
+            print('Yes')
+        else:
+            print('No')
+
+    @staticmethod
+    def message():
         msg = QtWidgets.QMessageBox()
-        msg.setIcon(QtWidgets.QMessageBox.Icon.Critical)
+        msg.setIcon(QtWidgets.QMessageBox.Icon.Warning)
         msg.setWindowTitle('title')
         msg.setInformativeText('description')
         msg.setDetailedText('detail')
-        msg.setStandardButtons(QtWidgets.QMessageBox.Ok | QtWidgets.QMessageBox.Cancel)
+        msg.setStandardButtons(
+            QtWidgets.QMessageBox.StandardButton.Ok |
+            QtWidgets.QMessageBox.StandardButton.Cancel |
+            QtWidgets.QMessageBox.StandardButton.Help
+        )
         item = msg.exec()
+        print(item.real)
 
-        if item == QtWidgets.QMessageBox.Ok:
+        if item == QtWidgets.QMessageBox.StandardButton.Ok:
             print('OK')
-        elif item == QtWidgets.QMessageBox.Cancel:
+        elif item == QtWidgets.QMessageBox.StandardButton.Cancel:
             print('CANCEL')
+        else:
+            print('HELP')
 
     def error_message(self):
         error = QtWidgets.QErrorMessage(self)
---
+++ lesson 8.2/main.py
@@ -0,0 +1,41 @@
+import sys
+
+from PyQt6 import QtCore, QtWidgets
+
+import qt_code
+import qt_code_wiget
+
+
+class GUI(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = qt_code.Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.open_modal_window)
+        self.ui.pushButton_2.clicked.connect(self.open_modal_window2)
+
+    def open_modal_window(self):
+        window = ModalWindow(self)
+        window.show()
+
+    def open_modal_window2(parent):
+
+        modal_window2 = QtWidgets.QWidget(parent, QtCore.Qt.WindowType.Window)
+        modal_window2.setWindowTitle('ModalW2')
+        modal_window2.resize(300, 500)
+        modal_window2.setWindowModality(QtCore.Qt.WindowModality.WindowModal)
+        modal_window2.show()
+
+class ModalWindow(QtWidgets.QWidget):
+    def __init__(self, parent=GUI):
+        super().__init__(parent, QtCore.Qt.WindowType.Window)
+        self.setWindowModality(QtCore.Qt.WindowModality.WindowModal)
+        self.ui = qt_code_wiget.Ui_Form()
+        self.ui.setupUi(self)
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    myapp = GUI()
+    myapp.show()
+    sys.exit(app.exec())
